1452d084674636780bca175724db6903e8d9d8cc
Yorleny <yorleny.a.v@gmail.com>
2023-05-10 10:29:44 -0600

    Variables

---
+++ PracticeVariables.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main()
+{
+    /* int grade1;
+    int grade2;
+    int average;
+
+    scanf("%d", &grade1);
+    scanf("%d", &grade2);
+
+    average = (grade1 + grade2) / 2;
+
+    printf("Average = %d\n", average); */
+
+    /* int currentYear;
+    int age;
+    int birthYear;
+
+    scanf("%d", &currentYear);
+    scanf("%d", &age);
+
+    birthYear = currentYear - age;
+
+    printf("You were born in %d", birthYear); */
+
+    /*double height;
+    double width;
+    double area;
+
+    printf("Enter the rectangle hight: ");
+    scanf("%lf", &height);
+    printf("Enter the rectangle width: ");
+    scanf("%lf", &width);
+
+    area = height * width;
+
+    printf("The area of the rectangle is %lf", area); */
+
+    /*float height, width, perimeter;
+
+    printf("Insert the height of the rectangle: ");
+    scanf("%f", &height);
+    printf("Insert the width of the rectangle: ");
+    scanf("%f", &width);
+
+    perimeter = (height + width) * 2;
+
+    printf("The perimeter of the rectangle is %.1f", perimeter); */
+
+    
+
+    return 0;
+}
